0792e0d5ce880fd688069385f5ea0ee3545f597a
Emmanuel Oscar <emmanuel001j@gmail.com>
2022-10-25 13:23:54 -0700

    update

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 #include <stdlib.h>
 /**
  * struct listint_s - singly linked list
@@ -14,16 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-/**
- * struct listadd_s - singly linked list
- * @address: Address of a node in a list
- * @next: Pointer to next node
- */
-typedef struct listadd_s
-{
-	void *address;
-	struct listadd_s *next;
-} listadd_t;
+size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -33,12 +24,11 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **, unsigned int index, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address);
-void free_listadd(listadd_t *head);
+void free_list(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-#endif
+#endif /* _LISTS_H_ */
